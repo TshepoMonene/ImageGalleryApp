632d3577df2f6d2573e5cf9f4b0110c317f0c429
TshepoMonene <Monenetshepo17@gmail.com>
2024-01-17 08:55:05 +0200

    Adding Model and dbcontext

---
+++ ImageGalleryAPI/ImageGalleryAPI/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImageGalleryAPI.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
---
+++ ImageGalleryAPI/ImageGalleryAPI/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿namespace ImageGalleryAPI.Models
+{
+    public class Photo
+    {   
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] UploadedImage { get; set; }
+
+        public DateTime  UploadDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public decimal Size { get; set;}
+
+    }
+}

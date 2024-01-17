49917993401a89c410a35bd8b8e85d024a4ad315
TshepoMonene <Monenetshepo17@gmail.com>
2024-01-17 21:44:41 +0200

    Adding User Interface

---
+++ ImageGalleryAPI/ImageGalleryWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ImageGalleryWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ImageGalleryWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ImageGalleryAPI/ImageGalleryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageGalleryWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
